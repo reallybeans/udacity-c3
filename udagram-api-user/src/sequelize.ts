ba95841b9c2ad59ad13d39af54f70679b23e2049
Bean <leeanh764@gmail.com>
2023-08-27 20:50:58 +0700

    change envvariable

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -7,7 +7,7 @@ export const sequelize = new Sequelize({
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
+  'port': 5432,
   'dialect': config.dialect,
   'storage': ':memory:',
 });
